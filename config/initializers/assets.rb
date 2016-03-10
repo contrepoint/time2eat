92ae4d3d2aa9d963085314d68ad75aee12cbbbbe
manjarb <manjarb4@gmail.com>
2016-03-10 18:43:24 +0800

    update layout

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( Good_Food_Display_-_NCI_Visuals_Online.jpg )
+Rails.application.config.assets.precompile += %w( Pringles_logo.png )
