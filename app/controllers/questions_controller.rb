4f566f38bd599246667b9ca9ac8fcc5e9694ba48
contrepoint <contrepoint21@gmail.com>
2016-03-16 09:59:55 +0800

    fix question controller bug

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,8 +8,9 @@ class QuestionsController < ApplicationController
   def create
   	q = Question.new(question_params)
   	q.group_id = params[:group_id]
+  	byebug
   	if q.save
-  		q_options_arr = params[:question_options].split(',').collect { |x| x.strip || x }
+  		q_options_arr = params[:question][:question_options].split(',').collect { |x| x.strip || x }
   		q_options_arr.each do |x|
   			options = QuestionOption.create(option_text: x, question_id: q.id)
   		end
